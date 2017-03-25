53c0fbcfd3c87b1d281f96e16989e189ccd9654e
Hyu <huy77bd@gmail.com>
2017-03-25 15:59:33 +0700

    Form lan 1

---
+++ QLThuVien/WindowsFormsApplication1/frm_capnhat.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frm_capnhat : Form
+    {
+        public frm_capnhat()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frm_capnhat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
