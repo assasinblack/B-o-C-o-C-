93d20348c198537a77a9ed8c2cd07b663394104c
dangkhoadt <dangkhoasn@gmail.com>
2017-03-29 20:48:00 +0700

    jkj

---
+++ QLThuVien/WindowsFormsApplication1/frrm_Theloaisach.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frrm_Theloaisach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frrm_Theloaisach));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnthem = new System.Windows.Forms.ToolStripButton();
+            this.btnsua = new System.Windows.Forms.ToolStripButton();
+            this.btnxoa = new System.Windows.Forms.ToolStripButton();
+            this.btnthoat = new System.Windows.Forms.ToolStripButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmatl = new System.Windows.Forms.TextBox();
+            this.txttentl = new System.Windows.Forms.TextBox();
+            this.bntluu = new System.Windows.Forms.Button();
+            this.dtgvtheloaisach = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvtheloaisach)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnthem,
+            this.btnsua,
+            this.btnxoa,
+            this.btnthoat});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(477, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnthem
+            // 
+            this.btnthem.Image = ((System.Drawing.Image)(resources.GetObject("btnthem.Image")));
+            this.btnthem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(62, 22);
+            this.btnthem.Text = "Thêm";
+            // 
+            // btnsua
+            // 
+            this.btnsua.Image = ((System.Drawing.Image)(resources.GetObject("btnsua.Image")));
+            this.btnsua.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnsua.Name = "btnsua";
+            this.btnsua.Size = new System.Drawing.Size(51, 22);
+            this.btnsua.Text = "Sửa";
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.Image = ((System.Drawing.Image)(resources.GetObject("btnxoa.Image")));
+            this.btnxoa.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(52, 22);
+            this.btnxoa.Text = "Xóa";
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Image = ((System.Drawing.Image)(resources.GetObject("btnthoat.Image")));
+            this.btnthoat.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(63, 22);
+            this.btnthoat.Text = "Thoát";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bntluu);
+            this.panel1.Controls.Add(this.txttentl);
+            this.panel1.Controls.Add(this.txtmatl);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(477, 65);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã thể loại:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên thể loại:";
+            // 
+            // txtmatl
+            // 
+            this.txtmatl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtmatl.Location = new System.Drawing.Point(90, 19);
+            this.txtmatl.Name = "txtmatl";
+            this.txtmatl.Size = new System.Drawing.Size(100, 26);
+            this.txtmatl.TabIndex = 2;
+            // 
+            // txttentl
+            // 
+            this.txttentl.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttentl.Location = new System.Drawing.Point(278, 19);
+            this.txttentl.Name = "txttentl";
+            this.txttentl.Size = new System.Drawing.Size(100, 26);
+            this.txttentl.TabIndex = 3;
+            // 
+            // bntluu
+            // 
+            this.bntluu.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntluu.Location = new System.Drawing.Point(393, 19);
+            this.bntluu.Name = "bntluu";
+            this.bntluu.Size = new System.Drawing.Size(75, 26);
+            this.bntluu.TabIndex = 4;
+            this.bntluu.Text = "Lưu";
+            this.bntluu.UseVisualStyleBackColor = true;
+            // 
+            // dtgvtheloaisach
+            // 
+            this.dtgvtheloaisach.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvtheloaisach.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgvtheloaisach.Location = new System.Drawing.Point(0, 90);
+            this.dtgvtheloaisach.Name = "dtgvtheloaisach";
+            this.dtgvtheloaisach.Size = new System.Drawing.Size(477, 204);
+            this.dtgvtheloaisach.TabIndex = 2;
+            // 
+            // frrm_Theloaisach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 294);
+            this.Controls.Add(this.dtgvtheloaisach);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frrm_Theloaisach";
+            this.Text = "frrm_Theloaisach";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvtheloaisach)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnthem;
+        private System.Windows.Forms.ToolStripButton btnsua;
+        private System.Windows.Forms.ToolStripButton btnxoa;
+        private System.Windows.Forms.ToolStripButton btnthoat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bntluu;
+        private System.Windows.Forms.TextBox txttentl;
+        private System.Windows.Forms.TextBox txtmatl;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dtgvtheloaisach;
+
+    }
+}
