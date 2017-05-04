f7226c70d3e6cfb57a45e41eb6af0e4fda7e3d3b
dangkhoadt <dangkhoasn@gmail.com>
2017-05-04 20:35:54 +0700

    a

--- QLThuVien/WindowsFormsApplication1/HeThong/frm_saoluuphuchoi.Designer.cs
+++ QLThuVien/WindowsFormsApplication1/HeThong/frm_saoluuphuchoi.Designer.cs
@@ -28,124 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.lblerr = new System.Windows.Forms.ToolStripStatusLabel();
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.btnsaoluu = new System.Windows.Forms.ToolStripButton();
-            this.tsbthoat = new System.Windows.Forms.ToolStripButton();
-            this.lblpanel = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtlayfile = new System.Windows.Forms.TextBox();
-            this.btnlayfile = new System.Windows.Forms.Button();
-            this.statusStrip1.SuspendLayout();
-            this.toolStrip1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtduongdan = new System.Windows.Forms.TextBox();
+            this.cbbackup = new System.Windows.Forms.CheckBox();
+            this.cbrestore = new System.Windows.Forms.CheckBox();
+            this.bntbackup = new System.Windows.Forms.Button();
+            this.bntrestore = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // statusStrip1
+            // label1
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.lblerr});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 108);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(580, 22);
-            this.statusStrip1.TabIndex = 0;
-            this.statusStrip1.Text = "statusStrip1";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(137, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Đường dẫn:";
             // 
-            // lblerr
+            // txtduongdan
             // 
-            this.lblerr.Name = "lblerr";
-            this.lblerr.Size = new System.Drawing.Size(0, 17);
+            this.txtduongdan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.txtduongdan.Location = new System.Drawing.Point(244, 79);
+            this.txtduongdan.Name = "txtduongdan";
+            this.txtduongdan.Size = new System.Drawing.Size(240, 23);
+            this.txtduongdan.TabIndex = 1;
             // 
-            // toolStrip1
+            // cbbackup
             // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnsaoluu,
-            this.tsbthoat});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(580, 25);
-            this.toolStrip1.TabIndex = 1;
-            this.toolStrip1.Text = "toolStrip1";
+            this.cbbackup.AutoSize = true;
+            this.cbbackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.cbbackup.Location = new System.Drawing.Point(147, 156);
+            this.cbbackup.Name = "cbbackup";
+            this.cbbackup.Size = new System.Drawing.Size(73, 21);
+            this.cbbackup.TabIndex = 2;
+            this.cbbackup.Text = "backup";
+            this.cbbackup.UseVisualStyleBackColor = true;
             // 
-            // btnsaoluu
+            // cbrestore
             // 
-            this.btnsaoluu.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
-            this.btnsaoluu.Image = global::WindowsFormsApplication1.Properties.Resources.Restore_Window_Filled_50;
-            this.btnsaoluu.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnsaoluu.Name = "btnsaoluu";
-            this.btnsaoluu.Size = new System.Drawing.Size(74, 22);
-            this.btnsaoluu.Text = "Sao lưu";
-            this.btnsaoluu.Click += new System.EventHandler(this.btnsaoluu_Click);
+            this.cbrestore.AutoSize = true;
+            this.cbrestore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(0)));
+            this.cbrestore.Location = new System.Drawing.Point(403, 156);
+            this.cbrestore.Name = "cbrestore";
+            this.cbrestore.Size = new System.Drawing.Size(72, 21);
+            this.cbrestore.TabIndex = 3;
+            this.cbrestore.Text = "restore";
+            this.cbrestore.UseVisualStyleBackColor = true;
             // 
-            // tsbthoat
+            // bntbackup
             // 
-            this.tsbthoat.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold);
-            this.tsbthoat.Image = global::WindowsFormsApplication1.Properties.Resources.Exit_Filled_50;
-            this.tsbthoat.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.tsbthoat.Name = "tsbthoat";
-            this.tsbthoat.Size = new System.Drawing.Size(63, 22);
-            this.tsbthoat.Text = "Thoát";
+            this.bntbackup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntbackup.Location = new System.Drawing.Point(147, 350);
+            this.bntbackup.Name = "bntbackup";
+            this.bntbackup.Size = new System.Drawing.Size(111, 30);
+            this.bntbackup.TabIndex = 4;
+            this.bntbackup.Text = "BACKUP";
+            this.bntbackup.UseVisualStyleBackColor = true;
             // 
-            // lblpanel
+            // bntrestore
             // 
-            this.lblpanel.BackColor = System.Drawing.Color.Turquoise;
-            this.lblpanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblpanel.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblpanel.Location = new System.Drawing.Point(0, 25);
-            this.lblpanel.Name = "lblpanel";
-            this.lblpanel.Size = new System.Drawing.Size(580, 33);
-            this.lblpanel.TabIndex = 2;
-            this.lblpanel.Text = "SAO LƯU";
-            this.lblpanel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 69);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(54, 22);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "URL:";
-            // 
-            // txtlayfile
-            // 
-            this.txtlayfile.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtlayfile.Location = new System.Drawing.Point(72, 67);
-            this.txtlayfile.Name = "txtlayfile";
-            this.txtlayfile.Size = new System.Drawing.Size(402, 29);
-            this.txtlayfile.TabIndex = 4;
-            // 
-            // btnlayfile
-            // 
-            this.btnlayfile.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnlayfile.Location = new System.Drawing.Point(480, 67);
-            this.btnlayfile.Name = "btnlayfile";
-            this.btnlayfile.Size = new System.Drawing.Size(88, 29);
-            this.btnlayfile.TabIndex = 5;
-            this.btnlayfile.Text = "Browers";
-            this.btnlayfile.UseVisualStyleBackColor = true;
-            this.btnlayfile.Click += new System.EventHandler(this.btnlayfile_Click);
+            this.bntrestore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntrestore.Location = new System.Drawing.Point(302, 350);
+            this.bntrestore.Name = "bntrestore";
+            this.bntrestore.Size = new System.Drawing.Size(111, 30);
+            this.bntrestore.TabIndex = 5;
+            this.bntrestore.Text = "RESTORE";
+            this.bntrestore.UseVisualStyleBackColor = true;
+            this.bntrestore.Click += new System.EventHandler(this.button2_Click);
             // 
             // frm_saoluuphuchoi
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(580, 130);
-            this.Controls.Add(this.btnlayfile);
-            this.Controls.Add(this.txtlayfile);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.lblpanel);
-            this.Controls.Add(this.toolStrip1);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(626, 453);
+            this.Controls.Add(this.bntrestore);
+            this.Controls.Add(this.bntbackup);
+            this.Controls.Add(this.cbrestore);
+            this.Controls.Add(this.cbbackup);
+            this.Controls.Add(this.txtduongdan);
+            this.Controls.Add(this.label1);
             this.Name = "frm_saoluuphuchoi";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Sao Lưu - Phục Hồi";
+            this.Text = "frm_saoluuphuchoi";
             this.Load += new System.EventHandler(this.frm_saoluuphuchoi_Load);
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,14 +118,11 @@
 
         #endregion
 
-        private System.Windows.Forms.StatusStrip statusStrip1;
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripButton btnsaoluu;
-        private System.Windows.Forms.ToolStripButton tsbthoat;
-        private System.Windows.Forms.Label lblpanel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtlayfile;
-        private System.Windows.Forms.Button btnlayfile;
-        private System.Windows.Forms.ToolStripStatusLabel lblerr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtduongdan;
+        private System.Windows.Forms.CheckBox cbbackup;
+        private System.Windows.Forms.CheckBox cbrestore;
+        private System.Windows.Forms.Button bntbackup;
+        private System.Windows.Forms.Button bntrestore;
     }
 }
--- QLThuVien/WindowsFormsApplication1/HeThong/frm_saoluuphuchoi.cs
+++ QLThuVien/WindowsFormsApplication1/HeThong/frm_saoluuphuchoi.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
 using System.Drawing;
-using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace WindowsFormsApplication1.HeThong
 {
     public partial class frm_saoluuphuchoi : Form
     {
-
-        private void btnlayfile_Click(object sender, EventArgs e)
+        public frm_saoluuphuchoi()
         {
-          
+            InitializeComponent();
         }
-
-        private void btnsaoluu_Click(object sender, EventArgs e)
+        private bool backuprestore()
         {
-
+            if (cbbackup.Checked == true)
+                return true;
+            else
+                return false;
         }
-
         private void frm_saoluuphuchoi_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
